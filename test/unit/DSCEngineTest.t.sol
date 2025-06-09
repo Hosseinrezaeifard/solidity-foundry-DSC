3e8bfc4adfd99467b40c87933414bb6e6f997bdf
HosseinRezaeiFard <huseinrezaeif99@gmail.com>
2025-06-09 13:08:02 -0700

    Implement first test

---
+++ test/unit/DSCEngineTest.t.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+
+contract DSCEngineTest is Test {
+    DSCEngine public dscEngine;
+    DecentralizedStableCoin public dsc;
+    DeployDSC public deployer;
+    HelperConfig public config;
+    address public ethUsdPriceFeed;
+    address public btcUsdPriceFeed;
+    address public weth;
+    address public wbtc;
+    uint256 public deployerKey;
+
+    function setUp() public {
+        deployer = new DeployDSC();
+        (dsc, dscEngine, config) = deployer.run();
+        (ethUsdPriceFeed, btcUsdPriceFeed, weth, wbtc, deployerKey) = config
+            .activeNetworkConfig();
+    }
+
+    /* ============================ Price Tests ============================ */
+    function testGetUsdValue() public {
+        uint256 ethAmount = 15e18;
+        // 15e18 * 2000/ETH = 30,000e18;
+        uint256 expectedUsd = 30000e18;
+        uint256 actualUsd = dscEngine.getUsdValue(weth, ethAmount);
+        assertEq(actualUsd, expectedUsd);
+    }
+}
