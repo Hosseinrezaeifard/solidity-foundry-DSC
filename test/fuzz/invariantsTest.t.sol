c4fb0b8d67d38a7e818ffbb7c76c2c341f58f56b
Hosseinrezaeifard <huseinrezaeif99@gmail.com>
2025-06-23 01:33:57 +0330

    Implement fuzz test handler structure

---
+++ test/fuzz/invariantsTest.t.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+// This file has our invariants aka system properties that should always hold
+
+// what are our invariants? (what are those properties that should always be true)
+// 1. The total supply of DSC(Debt) should be less than the total value of collateral
+// 2. Getter view functions should never revert => evergreen invariant
+
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol"
+
+contract InvariantsTest is StdInvariant, Test {
+    
+}
